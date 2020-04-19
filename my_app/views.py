13dbd4a164bd0d37f48440083e105ad07a618dd9
Srinidhi Madhusudhan <srinidhim.kattimani@gmail.com>
2020-04-19 21:47:13 +0530

    add forntend and requirements

--- my_app/views.py
+++ my_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
-
+import requests as re
+from bs4 import BeautifulSoup
 # Create your views here.
 def home(request):
     return render(request, 'my_app/base.html')
+
+def new_search(request):
+    search = request.POST.get('search')
+    print('search')
+    stuff_for_frontend = {
+        'search':search,
+    }
+    return render(request, 'my_app/new_search.html', stuff_for_frontend)
