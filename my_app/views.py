d5e2c4a67d010abfeb24d203073b502b5d1b9962
Srinidhi Madhusudhan <srinidhim.kattimani@gmail.com>
2020-04-20 12:24:20 +0530

    Apr 20 12:24PM

--- my_app/views.py
+++ my_app/views.py
@@ -1,13 +1,24 @@
 from django.shortcuts import render
 import requests as re
-from bs4 import BeautifulSoup
+from requests.compat import quote_plus
+from bs4 import BeautifulSoup as bs
+from . import models
+from django.utils import timezone
 # Create your views here.
+BASE_CRAIGLIST_URL = "https://bangalore.craigslist.org/search/?query={}"
 def home(request):
     return render(request, 'my_app/base.html')
 
 def new_search(request):
     search = request.POST.get('search')
-    print('search')
+    models.Search.objects.create(search = search)
+    final_url = BASE_CRAIGLIST_URL.format(quote_plus(search))
+    print(final_url)
+    response = re.get(final_url)
+    data = response.text
+    soup = bs(data, features='html.parser')
+    post_titles = soup.find_all('a',{'class':'result-title'})
+    print(post_titles)
     stuff_for_frontend = {
         'search':search,
     }
